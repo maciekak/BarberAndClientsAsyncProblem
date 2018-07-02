cc933d6ccfabfb9ef75379fa258aafbfe8e23064
Maciej Klimowski <maciek.klimowski44@gmail.com>
2018-07-02 16:49:51 +0200

    Renamed solution

--- BarberAndClientsAsync/Program.cs
+++ BarberAndClientsAsync/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BarberAndClientsAsyncTest
+namespace BarberAndClientsAsync
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
--- BarberAndClientsAsync/Properties/AssemblyInfo.cs
+++ BarberAndClientsAsync/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BarberAndClientsAsyncTest")]
+[assembly: AssemblyTitle("BarberAndClientsAsync")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BarberAndClientsAsyncTest")]
+[assembly: AssemblyProduct("BarberAndClientsAsync")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
